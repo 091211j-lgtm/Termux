7dbf4d1f428ca536a22276c830d8837bb9d9467c
JuanD <091211j@gmail.com>
2026-08-16 13:31:41 -0400

    Actualizar SQLMap.sh

--- SQLMap.sh
+++ SQLMap.sh
@@ -1,4 +1,7 @@
+echo "Instalando dependencias" 
 Pkg install python - y
+echo "Clonando repositorio" 
 git clone https://github.com/sqlmapproject/sqlmap.git
+echo "Instalando SQLMap" 
 cd sqlmap
 python sqlmap.py
